e8722ae73e93fee3a175911e012c886ee0e04796
Jace Browning <jacebrowning@gmail.com>
2017-11-05 19:35:04 -0500

    Fix spacing

--- {{cookiecutter.project_name}}/config/settings/base.py
+++ {{cookiecutter.project_name}}/config/settings/base.py
@@ -75,8 +75,8 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers':['console'],
-            'level':'INFO',
+            'handlers': ['console'],
+            'level': 'INFO',
         },
         '{{cookiecutter.project_name}}': {
             'handlers': ['console'],
