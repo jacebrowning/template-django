cc200b043234c380e8744243a2cdcd1733f55b79
Jace Browning <jacebrowning@gmail.com>
2018-01-14 16:38:00 -0500

    Update staging config to handle subdomains

--- {{cookiecutter.project_name}}/config/settings/staging.py
+++ {{cookiecutter.project_name}}/config/settings/staging.py
@@ -1,7 +1,11 @@
 from .production import *
 
 BASE_NAME = os.environ['HEROKU_APP_NAME']
-BASE_DOMAIN = f"{BASE_NAME}.herokuapp.com"
+if "-pr" in BASE_NAME:
+    BASE_DOMAIN = f"{BASE_NAME}.herokuapp.com"
+else:
+    BASE_NAME, SUBDOMAIN = BASE_NAME.split('-')
+    BASE_DOMAIN = f"{SUBDOMAIN}.{BASE_NAME}.com"
 BASE_URL = f"https://{BASE_DOMAIN}"
 
 ###############################################################################
