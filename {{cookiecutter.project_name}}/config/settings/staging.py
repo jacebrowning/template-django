81648440e011082008af3033bfe248c92311b102
Jace Browning <jacebrowning@gmail.com>
2018-01-14 16:52:13 -0500

    Handle domains with dashes

--- {{cookiecutter.project_name}}/config/settings/staging.py
+++ {{cookiecutter.project_name}}/config/settings/staging.py
@@ -4,7 +4,7 @@ BASE_NAME = os.environ['HEROKU_APP_NAME']
 if "-pr" in BASE_NAME:
     BASE_DOMAIN = f"{BASE_NAME}.herokuapp.com"
 else:
-    BASE_NAME, SUBDOMAIN = BASE_NAME.split('-')
+    BASE_NAME, SUBDOMAIN = BASE_NAME.rsplit('-', 1)
     BASE_DOMAIN = f"{SUBDOMAIN}.{BASE_NAME}.com"
 BASE_URL = f"https://{BASE_DOMAIN}"
 
