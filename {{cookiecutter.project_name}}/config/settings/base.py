097211dcbd96ada137c14b30132323f4eb4dfac2
Jace Browning <jacebrowning@gmail.com>
2018-02-17 21:13:16 -0500

    Add CORS middleware

--- {{cookiecutter.project_name}}/config/settings/base.py
+++ {{cookiecutter.project_name}}/config/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
