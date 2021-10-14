c00eb24a277e62b211079b6a4ac0b87f684ee907
Jace Browning <jacebrowning@gmail.com>
2021-10-14 19:28:32 -0400

    Use f-string

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/{{cookiecutter.first_app_name}}/views.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/{{cookiecutter.first_app_name}}/views.py
@@ -7,7 +7,5 @@ import log
 def current_datetime(request):
     log.debug(request)
     now = datetime.datetime.now()
-    html = ("<html><body>Welcome to {{cookiecutter.project_name}}."
-            "<br>"
-            "It is now %s.</body></html>") % now
+    html = f"<html><body>Welcome to {{cookiecutter.project_name}}.<br>It is now {now}.</body></html>"
     return HttpResponse(html)
